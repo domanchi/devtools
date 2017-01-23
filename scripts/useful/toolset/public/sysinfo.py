dfc94c45deee5d53803c9353b4a4646c2394ec13
Aaron <domanchi@umich.edu>
2017-01-22 19:32:50 -0800

    sysinfo: find out ubuntu distro

--- scripts/useful/toolset/public/sysinfo.py
+++ scripts/useful/toolset/public/sysinfo.py
@@ -7,6 +7,7 @@ class SystemInformationTool(AbstractTool):
 
     def usage(self):
         print "These tools are useful to find out info about your computer."
-        print "   uname -a          : display current system"
-        print "   groups <username> : display groups that the current user is in."
-        print "   gpasswd           : group management tool."
+        print "   uname -a           : display current system"
+        print "   groups <username>  : display groups that the current user is in."
+        print "   gpasswd            : group management tool."
+        print "   cat /etc/*-release : display Linux version."
