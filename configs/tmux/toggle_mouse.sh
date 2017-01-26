8eb387558caadb668a7a0bfbe92d9c254073a7fe
Aaron Loo <aaronloo@yelp.com>
2017-01-26 12:48:08 -0800

    upgraded toggle_mouse to v2

--- configs/tmux/toggle_mouse.sh
+++ configs/tmux/toggle_mouse.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-mouse_mode=$(tmux show -g | grep mouse | head -n 1 | cut -d ' ' -f2)
+# NOTE: Since this is running in a subshell, the alias of tmux==tmux2 won't work.
+mouse_mode=$(tmux2 show -g | grep mouse | head -n 1 | cut -d ' ' -f2)
 if [[ "$mouse_mode" == "on" ]]; then
-    tmux set -g mouse off \; display "mouse: OFF"
+    tmux2 set -g mouse off \; display "mouse: OFF"
 else
-    tmux set -g mouse on \; display "mouse: ON"
+    tmux2 set -g mouse on \; display "mouse: ON"
 fi
 unset mouse_mode
