34689779dc256240fa18debdb63e8df1ba5cb423
Aaron <domanchi@umich.edu>
2017-03-04 08:33:06 -0800

    merging

--- configs/tmux/toggle_mouse.sh
+++ configs/tmux/toggle_mouse.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # NOTE: Since this is running in a subshell, the alias of tmux==tmux2 won't work.
-mouse_mode=$(tmux2 show -g | grep mouse | head -n 1 | cut -d ' ' -f2)
+mouse_mode=$(tmux show -g | grep mouse | head -n 1 | cut -d ' ' -f2)
 if [[ "$mouse_mode" == "on" ]]; then
-    tmux2 set -g mouse off \; display "mouse: OFF"
+    tmux set -g mouse off \; display "mouse: OFF"
 else
-    tmux2 set -g mouse on \; display "mouse: ON"
+    tmux set -g mouse on \; display "mouse: ON"
 fi
 unset mouse_mode
--- scripts/useful/toolset/public/__init__.py
+++ scripts/useful/toolset/public/__init__.py
@@ -2,3 +2,4 @@ from sysinfo import *
 from web import *
 from crypto import *
 from vim import *
+from misc import *
