ab6b92702d8b3330cdb1b46d18d0ddaf6a7a1499
Aaron Loo <aaronloo@yelp.com>
2018-06-14 10:36:40 -0700

    new paradigm of bash scripts

---
+++ scripts/tmux_ssh_agent_fix.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# There is an issue with tmux and $SSH_AUTH_SOCK, where the tmux session
+# holds an invalid $SSH_AUTH_SOCK, making it harder to use ssh or git in
+# an existing tmux session.
+#
+# However, I have an issue where if the socket dies, you need to be able to
+# tell what the new socket is from within the tmux session. And this isn't
+# able to give you the latest results, because it doesn't know values
+# outside tmux.
+#
+# So fuck it. This script just fixes the symlink, and can be run manually
+# when a fix is needed.
+
+function main() {
+    # -n checks for non-zero string length
+    if [[ -n "$TMUX" ]]; then
+        echo "This should be run *outside* of tmux."
+        return 1
+    fi
+
+    # -e follows symlinks, and checks if file (at the end) exists
+    if [[ ! -e ~/.ssh/ssh_auth_sock ]]; then
+        ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+        echo "Fixed!"
+    fi
+
+    return 0
+}
+
+main
+
+
