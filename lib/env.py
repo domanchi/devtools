13f35f41d606580ea9920e2828b74bc3b11c1443
Aaron Loo <admin@aaronloo.com>
2020-04-15 07:28:03 -0700

    adding lib.vscode

--- lib/env.py
+++ lib/env.py
@@ -6,6 +6,8 @@ import os
 class OperatingSystem(Enum):
     UNKNOWN = None
     OSX = 'osx'
+    LINUX = 'ubuntu'
+    WINDOWS = 'wsl'
 
 
 @lru_cache(maxsize=1)
@@ -14,6 +16,8 @@ def get_environment() -> str:
     Attempts to heuristically obtain the operating system that
     this script is run under.
     """
+    # NOTE: We *can* use platform.system(), but on WSL, this would
+    #       return "Linux", which I'm not sure we currently want.
     if os.path.isfile('/usr/bin/sw_vers'):
         return OperatingSystem.OSX
 
