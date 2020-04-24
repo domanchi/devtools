d06c295ddfcb3b53c84b2fd70b08e5cc640f5e8f
Aaron Loo <admin@aaronloo.com>
2020-04-24 00:02:38 -0700

    moving vim install to lib/

--- lib/env.py
+++ lib/env.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from functools import lru_cache
 import os
+import warnings
 
 
 class OperatingSystem(Enum):
@@ -16,6 +17,15 @@ def get_environment() -> str:
     Attempts to heuristically obtain the operating system that
     this script is run under.
     """
+    if os.environ.get('DEVTOOLS_OS'):
+        try:
+            return OperatingSystem(os.environ.get('DEVTOOLS_OS'))
+        except ValueError:
+            warnings.warn(
+                'Passed in invalid environment variable: DEVTOOLS_OS. '
+                'Falling back to default behavior...'
+            )
+
     # NOTE: We *can* use platform.system(), but on WSL, this would
     #       return "Linux", which I'm not sure we currently want.
     if os.path.isfile('/usr/bin/sw_vers'):
--- lib/util.py
+++ lib/util.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 
 def get_path_to(path: str = '') -> str:
@@ -17,4 +18,24 @@ def force_create_symlink(src: str, dest: str):
     except FileExistsError:
         os.remove(dest)
         os.symlink(src, dest)
+
+
+def download_file(src: str, dest: str):
+    """
+    :param src: url to download
+    :param dest: path to save file
+    """
+    subprocess.call(
+        [
+            'curl',
+            '--fail',       # fail silently, on error
+            '--location',   # follow redirects
+            '--create-dirs',
+
+            '--output',
+            dest,
+            src,
+        ],
+        stderr=subprocess.DEVNULL,
+    )
 
