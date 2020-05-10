decca9531c616d64d244174d15a9d1792ba847da
Aaron Loo <admin@aaronloo.com>
2020-05-10 13:42:42 -0700

    bug fix: auto ssh-agent uses `ssh-add -l`
    On OSX, `SSH_AUTH_SOCK` is initialized via launchd. Therefore, `-z
    $SSH_AUTH_SOCK` does not work. Instead, we use `ssh-add -l` to list the
    current agents, and perform logic accordingly.

--- configs/common/bash/300.ssh-agent.sh
+++ configs/common/bash/300.ssh-agent.sh
@@ -1,7 +1,7 @@
 # Source: https://stackoverflow.com/a/18915067
 SSH_ENV="$HOME/.ssh/environment"
 
-function start-ssh-agent {
+function start_ssh_agent() {
     echo "Initializing new SSH Agent..."
     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "$SSH_ENV"
     echo "Succeeded!"
@@ -13,16 +13,17 @@ function start-ssh-agent {
 
 # For scenarios where the socket is already passed through (e.g. ssh -A),
 # we don't want to override it.
-if [[ -z "$SSH_AUTH_SOCK" ]]; then
+if [[ `ssh-add -l` == "The agent has no identities." ]]; then
     # Source SSH settings, if applicable
     if [[ -f "$SSH_ENV" ]]; then
         . "$SSH_ENV" > /dev/null
         ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-            start-ssh-agent;
+            start_ssh_agent;
         }
     else
-        start-ssh-agent;
+        start_ssh_agent;
     fi
 fi
 
 unset SSH_ENV
+unset start_ssh_agent
