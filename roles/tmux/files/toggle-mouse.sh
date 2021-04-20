3cf09d7aae334d129a7b31c446e0a651f75d7306
Aaron Loo <admin@aaronloo.com>
2021-04-20 11:05:12 -0700

    adding tmux role

--- roles/tmux/files/toggle-mouse.sh
+++ roles/tmux/files/toggle-mouse.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# NOTE: Since this is running in a subshell, the alias of tmux==tmux2 won't work.
 mouse_mode=$(tmux show -g | grep mouse | head -n 1 | cut -d ' ' -f2)
 if [[ "$mouse_mode" == "on" ]]; then
     tmux set -g mouse off \; display "mouse: OFF"
