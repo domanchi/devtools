8397ada3c099dc2a4acd42ae8570ac82477ed329
Aaron Loo <admin@aaronloo.com>
2021-04-21 12:38:51 -0700

    adding pass/fail icon to bash prompt

--- roles/bash/files/colorful-living.sh
+++ roles/bash/files/colorful-living.sh
@@ -1,5 +1,41 @@
 # Colorful bash
-export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\n\$ "
+export PROMPT_COMMAND=__prompt_command
+function __prompt_command() {
+    # Inspired by https://github.com/demure/dotfiles/blob/master/subbash/prompt
+    local EXIT="$?"     # this has to come first
+    PS1=''
+
+    # Colors
+    local ResetColor='\[\033[m\]'
+    local Yellow='\[\033[33;1m\]'
+    local Green='\[\033[32m\]'
+    local Blue='\[\033[36m\]'
+    local GreenBold='\[\e[1;92m\]'
+    local RedBold='\[\e[1;91m\]'
+
+    # Display current user
+    PS1+="$Blue\u$ResetColor"
+    PS1+="@"
+
+    # Display current hostname
+    PS1+="$Green\h$ResetColor"
+    PS1+=":"
+
+    # Display current path
+    PS1+="$Yellow\w$ResetColor"
+    PS1+=" "
+
+    # Show different icon on error.
+    if [[ "$EXIT" == 0 ]]; then
+        PS1+="${GreenBold}✔$ResetColor"
+    else
+        PS1+="${RedBold}✘$ResetColor"
+    fi
+
+    # Display prompt on new line
+    PS1+="\n\$ "
+}
+
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 
