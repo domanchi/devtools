34ce17cbdcb21e2e185545fa464b639912189945
Aaron Loo <admin@aaronloo.com>
2021-04-27 07:31:18 -0700

    bug fix: osx packages are all installed to a different namespace than "coreutils"

--- roles/osx/files/gnu-shims.sh
+++ roles/osx/files/gnu-shims.sh
@@ -1,7 +1,29 @@
-# Prefer GNU utils
-gnuToolsPath="$(brew --prefix coreutils)/libexec/gnubin"
-export PATH="$gnuToolsPath:$PATH"
-unset gnuToolsPath
+function getPathWithSuffix() {
+    # usage: getPath "<suffix>"
+    local suffix="$1"
+
+    # This list mirrors the homebrew installed packages as specified by the OSX role.
+    local packages=("coreutils" "findutils" "gnu-tar" "gnu-sed" "gnutls" "gnu-getopt" "grep")
+
+    local pkg
+    local output=""
+    for pkg in ${packages[@]}; do
+        if [[ "$output" ]]; then
+            output+=":"
+        fi
+
+        output+="$(brew --prefix $pkg)/libexec/$suffix"
+    done
+
+    echo "$output"
+}
+
+export PATH="`getPathWithSuffix 'gnubin'`:$PATH"
 
 # We want to point `man` to the correct location too.
-alias man='_() { local path="$(brew --prefix coreutils)/libexec/gnuman"; man -M $path $1 1>/dev/null 2>&1;  if [[ "$?" == 0 ]]; then man -M $path $1; else man $1; fi }; _'
+# We define it as such, to "hardcode" the path, so that the variable won't fall out of scope.
+# TODO: It seems that normal `man` is able to identify the right man pages based on correct setting
+# of path??
+# alias man="_() { local path="$(getPathWithSuffix 'gnuman')"; man -M \$path "\$1" 1>/dev/null 2>&1; if [[ "\$?" == 0 ]]; then man -M \$path "\$1"; else man "\$1"; fi }; _"
+
+unset getPathWithSuffix
