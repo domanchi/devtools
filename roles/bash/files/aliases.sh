05272eaf68d2b3168d67f7882f01b4bc0c2e81c7
Aaron Loo <admin@aaronloo.com>
2022-06-25 08:38:11 -0700

    adding bat (better cat)

--- roles/bash/files/aliases.sh
+++ roles/bash/files/aliases.sh
@@ -7,3 +7,7 @@ alias .....='cd ../../../..'
 # Default options (because, why wouldn't you?)
 alias cp='rsync --progress'     # for progress bar
 alias du='du --human-readable'
+
+# Adding aliases for newly installed, improved utilities
+# Source: https://rhodesmill.org/brandon/2009/commands-with-comma/
+alias ,cat="bat --paging=never"
